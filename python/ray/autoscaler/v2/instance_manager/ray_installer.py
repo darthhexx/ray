8ada891f2e7ddb39f49972269a15ea14e64f2acb
scv119 <scv119@gmail.com>
2023-05-02 16:05:17 -0700

    update

--- python/ray/autoscaler/v2/instance_manager/ray_installer.py
+++ python/ray/autoscaler/v2/instance_manager/ray_installer.py
@@ -3,7 +3,7 @@ import logging
 from ray.autoscaler._private.updater import NodeUpdater
 from ray.autoscaler._private.util import with_head_node_ip
 from ray.autoscaler.node_provider import NodeProvider as NodeProviderV1
-from ray.autoscaler.v2.instance_manager.node_config_provider import NodeConfigProvider
+from ray.autoscaler.v2.instance_manager.config import NodeProviderConfig
 from ray.core.generated.instance_manager_pb2 import Instance
 
 logger = logging.getLogger(__name__)
@@ -17,21 +17,19 @@ class RayInstaller(object):
     def __init__(
         self,
         provider: NodeProviderV1,
-        node_config_provider: NodeConfigProvider,
+        config: NodeProviderConfig,
     ) -> None:
         self._provider = provider
-        self._node_config_provider = node_config_provider
+        self._config = config
 
     def install_ray(self, instance: Instance, head_node_ip: str) -> bool:
         """
         Install ray on the target instance synchronously.
         """
 
-        setup_commands = self._node_config_provider.get_worker_setup_commands(instance)
-        ray_start_commands = self._node_config_provider.get_worker_start_ray_commands(
-            instance
-        )
-        docker_config = self._node_config_provider.get_docker_config(instance)
+        setup_commands = self._config.get_worker_setup_commands(instance)
+        ray_start_commands = self._config.get_worker_start_ray_commands(instance)
+        docker_config = self._config.get_docker_config(instance)
 
         logger.info(
             f"Creating new (spawn_updater) updater thread for node"
@@ -39,11 +37,11 @@ class RayInstaller(object):
         )
         updater = NodeUpdater(
             node_id=instance.instance_id,
-            provider_config=self._node_config_provider.get_config("provider"),
+            provider_config=self._config.get_config("provider"),
             provider=self._provider,
-            auth_config=self._node_config_provider.get_config("auth"),
-            cluster_name=self._node_config_provider.get_config("cluster_name"),
-            file_mounts=self._node_config_provider.get_config("file_mounts"),
+            auth_config=self._config.get_config("auth"),
+            cluster_name=self._config.get_config("cluster_name"),
+            file_mounts=self._config.get_config("file_mounts"),
             initialization_commands=with_head_node_ip(
                 self.get_node_type_specific_config(
                     instance.instance_id, "initialization_commands"
@@ -52,15 +50,13 @@ class RayInstaller(object):
             ),
             setup_commands=with_head_node_ip(setup_commands, head_node_ip),
             ray_start_commands=with_head_node_ip(ray_start_commands, head_node_ip),
-            runtime_hash=self._node_config_provider.runtime_hash,
-            file_mounts_contents_hash=self._node_config_provider.file_mounts_contents_hash,
+            runtime_hash=self._config.runtime_hash,
+            file_mounts_contents_hash=self._config.file_mounts_contents_hash,
             is_head_node=False,
-            cluster_synced_files=self._node_config_provider.get_config(
-                "cluster_synced_files"
-            ),
+            cluster_synced_files=self._config.get_config("cluster_synced_files"),
             rsync_options={
-                "rsync_exclude": self._node_config_provider.get_config("rsync_exclude"),
-                "rsync_filter": self._node_config_provider.get_config("rsync_filter"),
+                "rsync_exclude": self._config.get_config("rsync_exclude"),
+                "rsync_filter": self._config.get_config("rsync_filter"),
             },
             use_internal_ip=True,
             docker_config=docker_config,
